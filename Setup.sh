61ac8e0aa13622b9014f4545aba243132bd16c7f
Jonas Lara <61485427+jonas1ara@users.noreply.github.com>
2026-02-13 20:06:35 -0600

    Enhance Setup.sh with installation completion message
    Add completion message for installation process

--- Setup.sh
+++ Setup.sh
@@ -15,3 +15,11 @@ dotnet publish -c Release
 dotnet pack
 dotnet tool uninstall -g fitch
 dotnet tool install -g fitch --add-source nupkg
+
+# Back to root
+cd ..
+
+Write-Host " "
+Write-Host "Installation complete!"
+Write-Host "You can now run 'fitch' from anywhere in your terminal."
+Write-Host " "
